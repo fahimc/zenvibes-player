a5491cd039badca7b8ac665249de9e2be54214fe
Fahim Chowdhury <fahim.chowdhury1985@gmail.com>
2023-10-07 20:51:21 +0100

    UPDATED

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,12 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import styled from "styled-components";
+
+function downloadURI(uri, name) {
+  var link = document.createElement("a");
+  // If you don't know the name or want to use
+  // the webserver default set name = ''
+  link.setAttribute("download", name);
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  link.remove();
+}
 
 const Song = ({ currentSong }) => {
   return (
     <SongContainer>
       <Img src={currentSong.cover} alt={currentSong.name}></Img>
-      <H1>{currentSong.name}</H1>
-      <H2>{currentSong.artist}</H2>
+      <H1>{currentSong.name} </H1>
+      <H2>{currentSong.artist}</H2>{" "}
+      <FontAwesomeIcon
+        onClick={() => downloadURI(currentSong.src, currentSong.name + ".mp3")}
+        className="skip-back"
+        icon={faDownload}
+        style={{
+          cursor: "pointer",
+          width: "20px",
+          marginTop: "20px",
+        }}
+      />
     </SongContainer>
   );
 };
